536beb5c0fb8e3511046e7221eccd8f869de7564
Himanshu Gupta <119670803+himanshu1029g@users.noreply.github.com>
2023-09-18 05:29:46 +0000

    first day

---
+++ linked_treverse.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+int main()
+{
+
+// head = (struct Node *)malloc(sizeof(struct Node));
+
+        struct node *head;
+        struct node *second ;
+        struct node *third;
+        struct node *fourth;
+
+        head = (struct node *) malloc(sizeof(struct node));
+        head = (struct node *) malloc(sizeof(struct node));
+        head = (struct node *) malloc(sizeof(struct node));
+        head = (struct node *) malloc(sizeof(struct node));
+
+
+}
